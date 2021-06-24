bdb4bba80d4e41389cce3a8901ae30351970ab51
Ilya <mifomen@yandex.ru>
2021-06-25 01:22:40 +0300

    изменен header ffooter & map

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log('work')
+
+// let Links= document.getElementsByTagName('a');
+// console.log(Links)
+// for ( link of Links) {
+//   link.addEventListener('click',function (evt) {
+//     evt.preventDefault;
+//   })
+// }
+
+
+// let SliderControls = document.querySelectorAll('.label-slider')
+// let SliderControls = document.getElementsByTagName('input');
+// console.log(SliderControls)
+// for (Slider of SliderControls) {
+//   Slider.addEventListener('click',function (evt) {
+//     evt.preventDefault;
+//   })
+// }
